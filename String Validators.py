e85015977a0df5bcd5a169710f7937f0371a4879
Mbuso <mbuso@manjaro.homerouter.cpe>
2021-02-07 20:06:33 +0200

    Add the first hackerank files

---
+++ String Validators.py
@@ -0,0 +1,41 @@
+    '''
+    Task
+
+You are given a string .
+Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+Input Format
+
+A single line containing a string .
+
+Constraints
+
+
+Output Format
+
+In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+In the third line, print True if  has any digits. Otherwise, print False.
+In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+
+Sample Input
+
+qA2
+Sample Output
+
+True
+True
+True
+True
+True
+    '''
+ def string_validator():
+   
+    s = raw_input()
+    
+    print(any(x.isalnum() for x in s))
+    print(any(x.isalpha() for x in s))
+    print(any(x.isdigit() for x in s))
+    print(any(x.islower() for x in s))
+    print(any(x.isupper() for x in s))
